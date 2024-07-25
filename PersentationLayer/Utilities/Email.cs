3498333e48b5e4009beaf9d4085e00b93536c4cb
Youssef-Saeed-Hamed <youssefsaeedhamed9@gmail.com>
2024-07-25 23:34:12 +0300

    Add files via upload

---
+++ PersentationLayer/Utilities/Email.cs
@@ -0,0 +1,9 @@
+﻿namespace PersentationLayer.Utilities
+{
+	public class Email
+	{
+		public string Subject { get; set; }
+		public string Body { get; set; }
+		public string Recepient { get; set; }
+	}
+}
---
+++ PersentationLayer/Utilities/EmailSetting.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace PersentationLayer.Utilities
+{
+	public static  class EmailSetting
+	{
+		public static void SendEmail (Email email)
+		{
+			var Client = new SmtpClient("smtp.gmail.com", 587);
+			Client.EnableSsl = true;
+			Client.Credentials = new NetworkCredential("youssefsaeedhamed9@gmail.com", "keefwvoyllyqsqxn");
+			Client.Send("youssefsaeedhamed9@gmail.com", email.Recepient, email.Subject, email.Body);
+		}
+	}
+}
